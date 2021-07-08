489c8e8e72f68514c9569c58cccab2689a9c97d4
TarikCata <83212342+TarikCata@users.noreply.github.com>
2021-07-08 19:13:01 +0200

    Commit

---
+++ Designer Company/Rjesenje/wrd_files/script.js
@@ -0,0 +1,126 @@
+var nav = document.getElementById("nav");
+
+document.getElementById("IzbornikDugme").onclick = () => {
+    if (nav.style.maxHeight == '60px') {
+        nav.style.maxHeight = '390px';
+    }
+    else {
+        nav.style.maxHeight = '60px';
+    }
+}
+
+$.validator.addMethod(
+    "regex",
+    function (value, element, regexp) {
+        var check = false;
+        return this.optional(element) || regexp.test(value);
+    },
+    "Molim vas provjerite vaš unos!"
+);
+
+
+var forma = $("#forma");
+forma.validate({
+    rules: {
+        naslov: {
+            required: true,
+            regex: /^[A-Z]{1}[a-zA-Z ]+[a-zA-Z]+$/
+        },
+        telefon: {
+            required: true,
+            regex:/^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{4}$/
+        },
+        messages: {
+            naslov: {
+                required: "Ovo polje je obavezno",
+                regex: "Prva dva slova moraju biti velika slova"
+            },
+            telefon: {
+                required: "Ovo polje je obavezno",
+                regex: "Treba biti u formatu:+111-11-111-1111"
+            }
+        }
+    }
+})
+
+
+var allStudents;
+//1
+makeRedove = (data) => {
+    return `<div id="wrapperZaSliku">
+<img id="zaPng" src="${data.slikaPutanja}" />
+<div id="zaTxt">
+<h3>${data.imePrezime}</h3>
+<p>${data.opis}</p>
+<button class="buttonSend">Piši poruku</button>
+</div>
+</div>`
+}
+
+get4student = () => {
+    fetch('https://restapiexample.wrd.app.fit.ba/Ispit20210702/Get4Studenta')
+        .then(res => res.json()).then(data => {
+            console.table(data);
+            for (const i in data) {
+                document.querySelector("#forPicture").innerHTML += makeRedove(data[i]);
+                allStudents = data;
+            }
+            addClickListeners();//zanemari
+        })
+        .catch(err => console.log(err));
+}
+get4student();
+
+
+
+//3
+postSlanje = (e) => {
+    e.preventDefault();
+    data = {
+        id: allStudents.id,
+        imePrezime: $("#primaoc").val(),
+        naslov: $("#naslov").val(),
+        poruka: $("#poruka").val(),
+        datumVrijeme: allStudents.datumVrijeme,
+        telefon: $("#telefon").val(),
+    }
+    fetch('https://restapiexample.wrd.app.fit.ba/Ispit20210702/Add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => {
+            if (res.status !== 200) {
+                console.log("palo");
+            }
+        })
+        res.json().then(data =>{
+            console.log(data)
+        })
+
+        .catch(err => console.log(err));
+}
+
+//za provjeru da li je dodano preko post metode
+provjeri = () => {
+    fetch('https://restapiexample.wrd.app.fit.ba/Ispit20210702/Get')
+        .then(res => res.json()).then(data => {
+            console.table(data);
+        })
+        .catch(err => console.log(err));
+}
+provjeri();
+
+
+//zanemari
+let addClickListeners = () => {
+    let buttons = document.getElementsByClassName('buttonSend');
+    for (let i = 0; i < buttons.length; i++) {
+        const element = buttons[i];
+        element.addEventListener('click', () => {
+            console.log('test')
+        })
+    }
+}
