b98225236aa1945a1ecef5070c53220ad1cd3eae
TarikCata <83212342+TarikCata@users.noreply.github.com>
2021-10-16 13:18:54 +0200

    Update js

--- Iznajmljivanje Nekretnina/Rjesenje/folderi/javascript.js
+++ Iznajmljivanje Nekretnina/Rjesenje/folderi/javascript.js
@@ -1,129 +1,134 @@
 var dugme = document.getElementById("IzbornikDugme");
 var nav = document.getElementById("nav");
 
-dugme.onclick=()=>{
-    if(nav.style.maxHeight == '66px'){
-        nav.style.maxHeight = '320px';
-    }
-    else{
-        nav.style.maxHeight = '66px';
-    }
-}
+dugme.onclick = () => {
+  if (nav.style.maxHeight == "66px") {
+    nav.style.maxHeight = "320px";
+  } else {
+    nav.style.maxHeight = "66px";
+  }
+};
 
-var okvir = document.getElementsByClassName("VilaKolonaOkvir");
+const okvir = document.getElementsByClassName("VilaKolonaOkvir");
 
-for(let i=0;i<okvir.length;i++){
-    okvir[i].onmouseover = hoveruj;
-    okvir[i].onmouseout = iskljuci;
-
+for (let i = 0; i < okvir.length; i++) {
+  okvir[i].onmouseover = hoveruj;
+  okvir[i].onmouseout = iskljuci;
 }
 
 function hoveruj() {
-    this.style.border = '10px solid yellow';
+  this.style.border = "10px solid yellow";
 }
 
 function iskljuci() {
-    this.style.border = 'none';
+  this.style.border = "none";
 }
 
-
 $.validator.addMethod(
-    "regex",
-    function(value, element, regexp) {
-        var check = false;
-        return this.optional(element) || regexp.test(value);
-    },
-    "Unos nije validan!"
+  "regex",
+  function (value, element, regexp) {
+    var check = false;
+    return this.optional(element) || regexp.test(value);
+  },
+  "Unos nije validan!"
 );
 
 var forma = $("#forma");
 
 forma.validate({
-    rules:{
-        dostavaIme:{
-            required:true,
-            regex:/^[a-zA-Z]+$/
-        },
-        dostavaAdresa:{
-            required:true,
-            regex:/^[a-zA-Z]+$/
-        },
-        dostavaPostanskiBroj:{
-            required:true,
-            regex:/^[0-9]{5,}$/
-        },
-        dostavaTelefon:{
-            required:true,
-            regex:/^[0-9]{5,}$/
-        }
-        
+  rules: {
+    dostavaIme: {
+      required: true,
+      regex: /^[a-zA-Z]+$/,
     },
-    messges:{
-        dostavaIme:{
-            required:"Ovo polje je obavezno",
-            
-        },
-        dostavaAdresa:{
-            required:"Ovo polje je obavezno",
-            
-        },
-        dostavaPostanskiBroj:{
-            required:"Ovo polje je obavezno",
-            regex:"Minimalno 5 brojeva"
-        },
-        dostavaTelefon:{
-            required:"Ovo polje je obavezno",
-            regex:"Treba bit u formatu -> : +111-11-111-1111"
-        }
-    }
-    
+    dostavaAdresa: {
+      required: true,
+      regex: /^[a-zA-Z]+$/,
+    },
+    dostavaPostanskiBroj: {
+      required: true,
+      regex: /^[0-9]{5,}$/,
+    },
+    dostavaTelefon: {
+      required: true,
+      regex: /^[0-9]{5,}$/,
+    },
+  },
+  messges: {
+    dostavaIme: {
+      required: "Ovo polje je obavezno",
+    },
+    dostavaAdresa: {
+      required: "Ovo polje je obavezno",
+    },
+    dostavaPostanskiBroj: {
+      required: "Ovo polje je obavezno",
+      regex: "Minimalno 5 brojeva",
+    },
+    dostavaTelefon: {
+      required: "Ovo polje je obavezno",
+      regex: "Treba bit u formatu -> : +111-11-111-1111",
+    },
+  },
 });
-makeRedpve=(obj)=>{
-    return `<tr>
-    <td>${obj.narudzbaId}</td>
-    <td>${obj.datumNarudzbe}</td>
-    <td>${obj.dostavaIme}</td>
-    <td>${obj.dostavaAdresa}</td>
-    <td>${obj.dostavaPostanskiBroj}</td>
-    <td>${obj.dostavaTelefon}</td>
-    <td>${obj.napomena}</td>
+
+makeRedove = (obj) => {
+  const {
+    narudzbaId,
+    datumNarudzbe,
+    dostavaAdresa,
+    dostavaIme,
+    dostavaPostanskiBroj,
+    dostavaTelefon,
+    napomena,
+  } = obj;
+
+  return `<tr>
+    <td>${narudzbaId}</td>
+    <td>${datumNarudzbe}</td>
+    <td>${dostavaIme}</td>
+    <td>${dostavaAdresa}</td>
+    <td>${dostavaPostanskiBroj}</td>
+    <td>${dostavaTelefon}</td>
+    <td>${napomena}</td>
     </td>`;
-}
-getAll=()=>{
-    fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/GetAll')
-    .then(res => res.json())
-    .then(data=>{
-        for (const i in data) {
-            document.querySelector("#tabelaID").innerHTML+=makeRedpve(data[i]);
-        }
-    })
-    .catch(err =>{
-        console.log("err");
-    })
-}
+};
+
+const urlAll =
+  "http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/GetAll";
+
+const urlPost =
+  "http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj";
+
+getAll = async () => {
+  try {
+    const response = await fetch(urlAll);
+    const data = await response.json();
+    for (const i in data) {
+      document.querySelector("#tabelaID").innerHTML += makeRedove(data[i]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 getAll();
 
-document.getElementById("dodaj").onclick=(event)=>{
-    event.preventDefault();
-   
-    obj={
-        dostavaAdresa:$("#dostavaAdresa").val(),
-        dostavaIme:$("#dostavaIme").val(),
-        dostavaPostanskiBroj:$("#dostavaPostanskiBroj").val(),
-        dostavaTelefon:$("#dostavaTelefon").val(),
-        napomena:$("#napomena").val(),
-    }
+postF = (e) => {
+  e.preventDefault();
+  obj = {
+    dostavaAdresa: $("#dostavaAdresa").val(),
+    dostavaIme: $("#dostavaIme").val(),
+    dostavaPostanskiBroj: $("#dostavaPostanskiBroj").val(),
+    dostavaTelefon: $("#dostavaTelefon").val(),
+    napomena: $("#napomena").val(),
+  };
 
-    fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj',{
-        method:'post',
-        body:JSON.stringify(obj)
+  fetch(urlPost, {
+    method: "post",
+    body: JSON.stringify(obj),
+  })
+    .then((res) => {
+      console.log("Poslano " + res);
     })
-    .then(res => res.json())
-    .then(data=>{
-        console.log(data);
-        getAll();
-    })
-    .catch( err =>{
-        console.log("err");
-    })
-}
+    .catch((err) => console.log(err));
+};
