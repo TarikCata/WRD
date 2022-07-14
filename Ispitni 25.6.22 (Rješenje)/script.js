7b95a5a44498f90610870e77436eb48a39d4039d
TarikCata <83212342+TarikCata@users.noreply.github.com>
2022-07-14 15:46:27 +0200

    add validacija

--- Ispitni 25.6.22 (Rješenje)/script.js
+++ Ispitni 25.6.22 (Rješenje)/script.js
@@ -1,3 +1,33 @@
+$.validator.addMethod(
+  "regex",
+  function (value, element, regexp) {
+    var check = false;
+    return this.optional(element) || regexp.test(value);
+  },
+  "Molim vas provjerite vaš unos!"
+);
+
+var forma = $("#contact-form");
+var validacija = forma.validate({
+  rules: {
+    ime: {
+      required: true,
+      regex: /^[A-Z][a-z]+$/,
+    },
+    lastName: {
+      required: true,
+      regex: /^[A-Z][a-z]+$/,
+    },
+    email: {
+      required: true,
+      regex: /^[A-Za-z]+\.?[A-Za-z]+@(edu.fit.ba|hotmail.com|gmail.com)$/,
+    },
+    phone: {
+      required: true,
+      regex: /^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{3}$/,
+    },
+  },
+});
 var svi = [];
 idZaPoslat = "";
 makeRedove = (obj) => {
@@ -5,13 +35,14 @@ makeRedove = (obj) => {
     <img src=${obj.slikaUrl} alt="offer" />
     <div class="offer-details">
     <div class="cijena-naziv">
-        <h4 class="offer-destination">${obj.drzava}</h4>
-        <h3 class="offer-price">$${obj.cijena}</h3>
+    <h4 class="offer-destination">${obj.drzava}</h4>
+    <h3 class="offer-price">$${obj.cijena}</h3>
     </div>
     </div>
     <div class="ponudaa"><button onclick="upisiDestinaciju(${obj.id})" class="ponuda-dugme">Odaberi ponudu</button></div>
     </article>`;
 };
+console.log(validacija);
 getPonude = async () => {
   const url = "https://restapiexample.wrd.app.fit.ba/Ispit20220625/Get6Ponuda";
   const response = await fetch(url);
@@ -30,35 +61,60 @@ upisiDestinaciju = (id) => {
     }
   }
 };
+
+provjera = () => {
+  var vrijednosti = [
+    document.getElementById("first-name").value,
+    document.getElementById("email").value,
+    document.getElementById("last-name").value,
+    document.getElementById("phone").value,
+  ];
+  if (idZaPoslat === "") {
+    alert("Morate odabrati destinaciju");
+    return false;
+  }
+  for (const i of vrijednosti) {
+    if (i === "") {
+      console.log("prazno");
+      return false;
+    }
+  }
+  return true;
+};
 posalji = () => {
-  const urlPost = "https://restapiexample.wrd.app.fit.ba/Ispit20220625/Add";
-  const zaPoslat = {
-    destinacijaID: idZaPoslat,
-    ime: $("#first-name").val(),
-    prezime: $("#last-name").val(),
-    poruka: $("#poruka").val(),
-    email: $("#email").val(),
-    telefon: $("#phone").val(),
-  };
-  fetch(urlPost, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(zaPoslat),
-  })
-    .then((res) => res.json())
-    .then((obj) => {
-      alert(
-        "Poruka:" +
-          obj.poruka +
-          "\nBroj Rezervacija:" +
-          obj.brojRezervacije +
-          "\nVrijeme:" +
-          obj.vrijeme
-      );
+  if (!provjera()) {
+    return;
+  }
+  if (validacija.errorList.length === 0) {
+    const urlPost = "https://restapiexample.wrd.app.fit.ba/Ispit20220625/Add";
+    const zaPoslat = {
+      destinacijaID: idZaPoslat,
+      ime: $("#first-name").val(),
+      prezime: $("#last-name").val(),
+      poruka: $("#poruka").val(),
+      email: $("#email").val(),
+      telefon: $("#phone").val(),
+    };
+    fetch(urlPost, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(zaPoslat),
     })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((res) => res.json())
+      .then((obj) => {
+        alert(
+          "Poruka:" +
+            obj.poruka +
+            "\nBroj Rezervacija:" +
+            obj.brojRezervacije +
+            "\nVrijeme:" +
+            obj.vrijeme
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 };
